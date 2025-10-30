9b2a286f8b3dee90852b05b8f726539c92295ff6
CheeZe Dark(Rikko Matsumato) <cheezedarkofficial@gmail.com>
2025-10-30 21:12:03 +0200

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import PyBass.bass as b
+import os.path as p
+import time
+from os import _exit as exit
+def GetFoundedMusic(musicname : str):
+    if(p.exists(musicname)):
+        return True
+    else:
+        return False
+
+def SetGitDownloadAndCreateFolder(git_websitesrccode : str, env_name, foldername):
+    os.environ[env_name] = foldername
+    os.system("mkdir {}".format(os.environ[env_name]))
+    os.system("git clone {} --recurse-submodules {}".format(git_websitesrccode, os.environ[env_name]))
+    if(p.exists(os.environ[env_name] + "\\msvc\\mono.sln")):
+        print("You Need to write in Manual this Command inside Downloaded Mono from Unity3D: git submodule update --init --recursive \nIt is needed to update submodules!!!")
+        time.sleep(4)
+        os.system("start devenv.exe {}".format(os.environ[env_name] + "\\msvc\\mono.sln"))
+    else:
+        print("Failed to Found mono.sln... Please Try Again or Run this as Administrator and make sure what Visual Studio 2022 is in System Environment!!!")
+        time.sleep(4)
+        exit(221)
+
+def PlayMusic(filename):
+    if(b.BASS_INIT(device=-1, freq=44100, flags=0, win=0, dsguid=0)):
+        b.BASS_START()
+        stream_file = b.BASS_StreamCreateFile(mem=0, filename=str(filename).encode("utf-8"), offset=0, length=0, flags=0x4)
+        b.BASS_ChannelPlay(stream_file, False)
+    else:
+        print("Failed to Init Un4Seen Bass!!!")
+        time.sleep(10)
+        exit(443)
+
+def Main():
+    if GetFoundedMusic("MrRobotMainMenu.mp3"):
+        PlayMusic("MrRobotMainMenu.mp3")
+        SetGitDownloadAndCreateFolder("https://github.com/Unity-Technologies/mono.git", "MONO_UNITY3DFOLDER_SRCCODE", "MonoFromUnity3D\\mono")
+    else:
+        exit(4435)
+
+if __name__ == "__main__":
+    Main()
